68717befe02427d2d1ed9c81b28e70a240411ffc
Timofte Cosmin <timoftecosmin@yahoo.com>
2024-06-25 13:52:53 +0000

    Wire up URLs and views for homepage

--- tablehub/urls.py
+++ tablehub/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include("booking.urls"), name='booking-urls'),
+    path('', include('booking.urls'), name='booking-urls'),
+    path('', include('home.urls'), name='home-urls' ),
     path('admin/', admin.site.urls),
 ]
