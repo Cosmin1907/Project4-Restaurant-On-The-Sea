91829e4da1d64d4a03ddddfb61a8b701404a66b0
Timofte Cosmin <timoftecosmin@yahoo.com>
2024-06-26 09:32:26 +0000

    Install and wire up Allauth

--- tablehub/urls.py
+++ tablehub/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('booking.urls'), name='booking-urls'),
-    path('', include('home.urls'), name='home-urls' ),
     path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path('booking/', include('booking.urls'), name='booking-urls'),
+    path('', include('home.urls'), name='home-urls' ),
 ]
