a05704795e3b9c632c977ffba409665a2c752ca7
Timofte Cosmin <timoftecosmin@yahoo.com>
2024-06-27 09:22:41 +0000

    Update admin configuration for Booking and Table

--- booking/admin.py
+++ booking/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Table, Booking
 
 # Register your models here.
-admin.site.register(Table)
-admin.site.register(Booking)
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone_number', 'user', 'date', 'time_slot', 'no_of_guests', 'booking_notes']
+    list_filter = ['date', 'time_slot']
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ['table_number', 'capacity', 'table_notes']
+
