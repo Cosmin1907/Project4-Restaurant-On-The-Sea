8fe7a06ef5f8bcc7e8fe1eccbb3ca966ca0f1ff2
Timofte Cosmin <timoftecosmin@yahoo.com>
2024-07-01 12:14:13 +0000

    Add date picker widget to BookingForm

--- booking/forms.py
+++ booking/forms.py
@@ -5,3 +5,7 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ('name', 'phone_number', 'date', 'time_slot', 'no_of_guests', 'booking_notes')
+        # Source: https://docs.djangoproject.com/en/4.2/ref/forms/widgets/
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),  
+        }
