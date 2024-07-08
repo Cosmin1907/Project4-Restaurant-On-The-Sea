c9d0a538995becd1a6e2b42e32044be3c274cc01
Timofte Cosmin <timoftecosmin@yahoo.com>
2024-07-08 08:56:49 +0000

    Add validation for max tables per date/time

--- booking/forms.py
+++ booking/forms.py
@@ -1,6 +1,7 @@
 import random
 from .models import Booking, Table
 from django import forms
+from django.core.exceptions import ValidationError
 
 class BookingForm(forms.ModelForm):
     class Meta:
@@ -14,10 +15,15 @@ class BookingForm(forms.ModelForm):
     def save(self, commit=True):
         # Get an unsaved instance of the model
         booking = super().save(commit=False)
+
+        # Count existing tables for the given date and time slot
+        existing_bookings = Booking.objects.filter(date=booking.date, time_slot=booking.time_slot).count()
+        if existing_bookings >= 2:
+            raise ValidationError("No tables available for this date and time")
         
         # Find or create a table with the required capacity
         if not booking.booked_table:
-            table_number = random.randint(1, 20)
+            table_number = random.randint(1, 2)
             table = Table.objects.create(table_number=table_number, capacity=booking.no_of_guests)
                
             booking.booked_table = table
