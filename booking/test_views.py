d7942167e5f34d25f5a4e42e303ceb38709ccbb0
Timofte Cosmin <timoftecosmin@yahoo.com>
2024-07-17 09:25:16 +0000

    Add test for booking list view

--- booking/test_views.py
+++ booking/test_views.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from .forms import BookingForm
 from .models import Booking
 from django.test import Client
+from datetime import date, timedelta
 
 
 # Create your tests here.
@@ -13,7 +14,7 @@ from django.test import Client
 class TestBookingViews(TestCase):
     def setUp(self):
         self.client = Client()
-        
+
         # Create a superuser
         self.superuser = User.objects.create_superuser(
             username="admin",
@@ -30,7 +31,7 @@ class TestBookingViews(TestCase):
 
         # Create bookings for each user
         self.booking1 = Booking.objects.create(
-            name="Admin",
+            name="Admin Booking",
             user=self.superuser,
             phone_number="+40723974593",
             date=date.today() + timedelta(days=1),  
@@ -39,13 +40,30 @@ class TestBookingViews(TestCase):
         )
 
         self.booking2 = Booking.objects.create(
-            name="User",
+            name="User Booking",
             user=self.regular_user,
             phone_number="+40723974587",
             date=date.today() + timedelta(days=1),  
             time_slot=2,
             no_of_guests=2
         )
+
+    def test_booking_list_view(self):
+        # Log in as superuser and test
+        self.client.login(username="admin", password="adminPass")
+        response = self.client.get(reverse('booking-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'booking/booking_list.html')
+        self.assertContains(response, 'Made by: Admin Booking')
+        self.assertContains(response, 'Made by: User Booking')
+
+        # Log in as regular user and test
+        self.client.login(username='user', password='userPass')
+        response = self.client.get(reverse('booking-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'booking/booking_list.html')
+        self.assertContains(response, 'Made by: User Booking')
+        self.assertNotContains(response, 'Made by: Admin Booking')
 
 
         
