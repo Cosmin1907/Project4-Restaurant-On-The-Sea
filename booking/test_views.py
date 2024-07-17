66536339e467432ef24159223a513f877265ce4c
Timofte Cosmin <timoftecosmin@yahoo.com>
2024-07-17 08:47:55 +0000

    Setup test client and update user bookings

--- booking/test_views.py
+++ booking/test_views.py
@@ -3,28 +3,49 @@ from django.urls import reverse
 from django.test import TestCase
 from .forms import BookingForm
 from .models import Booking
+from django.test import Client
 
 
 # Create your tests here.
+# Part of the coude inspired by Source: 
+# https://docs.djangoproject.com/en/5.0/topics/testing/tools/#the-test-client
 
 class TestBookingViews(TestCase):
     def setUp(self):
+        self.client = Client()
+        
         # Create a superuser
-        self.user = User.objects.create_superuser(
-            username="myUsername",
-            password="myPassword",
-            email="test@test.com"
+        self.superuser = User.objects.create_superuser(
+            username="admin",
+            password="adminPass",
+            email="admin@test.com"
         )
 
-        # Log in the user
-        self.client.login(username="myUsername", password="myPassword")
+        # Create regular user
+        self.regular_user = User.objects.create_user(
+            username="user",
+            password="userPass",
+            email="user@test.com"
+        )
 
-        # Create a booking instance
-        self.booking = Booking.objects.create(
-            name="Name",
+        # Create bookings for each user
+        self.booking1 = Booking.objects.create(
+            name="Admin",
+            user=self.superuser,
             phone_number="+40723974593",
             date=date.today() + timedelta(days=1),  
             time_slot=1,
             no_of_guests=1
         )
+
+        self.booking2 = Booking.objects.create(
+            name="User",
+            user=self.regular_user,
+            phone_number="+40723974587",
+            date=date.today() + timedelta(days=1),  
+            time_slot=2,
+            no_of_guests=2
+        )
+
+
         
