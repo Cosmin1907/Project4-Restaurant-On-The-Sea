9639d1bb7dfd82b758c257179f7f94f8a563161b
Timofte Cosmin <timoftecosmin@yahoo.com>
2024-07-22 13:29:48 +0000

    Add new  models to admin pannel

--- rating/admin.py
+++ rating/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Rating, Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('header', 'text')
+    search_fields = ('header', 'text')
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'rating')
+    list_filter = ('post', 'rating')
+    search_fields = ('user__username', 'post__header')
