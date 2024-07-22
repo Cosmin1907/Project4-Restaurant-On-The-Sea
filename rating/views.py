a9a16b5fc2fa978306b461c06b53c9389a528225
Timofte Cosmin <timoftecosmin@yahoo.com>
2024-07-22 08:51:13 +0000

    Add views for post and rate

--- rating/views.py
+++ rating/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    posts = Post.objects.all()
+    for post in posts:
+        rating = Rating.objects.filter(post=post, user=request.user).first()
+        post.user_rating = rating.rating if rating else 0
+    return render(request, "index.html", {"posts": posts})
+
+def rate(request: HttpRequest, post_id: int, rating: int) -> HttpResponse:
+    post = Post.objects.get(id=post_id)
+    Rating.objects.filter(post=post, user=request.user).delete()
+    post.rating_set.create(user=request.user, rating=rating)
+    return index(request)
