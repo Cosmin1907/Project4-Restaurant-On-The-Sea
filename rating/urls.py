54211d17dcec7e31f0bc844377593aca62e3d95a
Timofte Cosmin <timoftecosmin@yahoo.com>
2024-07-22 12:15:46 +0000

    Update url connectios for rating app

--- rating/urls.py
+++ rating/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('rate/<int:post_id>/<int:rating>/', views.rate),
     path('', views.index),
 ]
--- rating/views.py
+++ rating/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from .models import Rating, Post
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
@@ -7,7 +8,7 @@ def index(request: HttpRequest) -> HttpResponse:
     for post in posts:
         rating = Rating.objects.filter(post=post, user=request.user).first()
         post.user_rating = rating.rating if rating else 0
-    return render(request, "index.html", {"posts": posts})
+    return render(request, "rating/ratings.html", {"posts": posts})
 
 def rate(request: HttpRequest, post_id: int, rating: int) -> HttpResponse:
     post = Post.objects.get(id=post_id)
