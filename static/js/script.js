23f5282b491ff9267c0f9746ca35f06903a5af18
Timofte Cosmin <timoftecosmin@yahoo.com>
2024-07-24 13:46:22 +0000

    Fix js bug

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 const rate = (rating, post_id) => {
-    fetch(`rate/${post_id}/${rating}/`, {
+    fetch(`/rating/rate/${post_id}/${rating}/`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
