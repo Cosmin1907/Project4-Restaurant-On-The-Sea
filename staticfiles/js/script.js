1bf2a43d8d61d1dadf6b1a027d807db078f18850
Timofte Cosmin <timoftecosmin@yahoo.com>
2024-08-01 05:44:51 +0000

    Update static collection

---
+++ staticfiles/js/script.js
@@ -0,0 +1,14 @@
+// Source: https://medium.com/geekculture/django-implementing-star-rating-e1deff03bb1c
+const rate = (rating, post_id) => {
+    fetch(`/rating/rate/${post_id}/${rating}/`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            window.location.reload();
+        }
+    });
+}
